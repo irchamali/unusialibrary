79a2b5202a7f07405e5d5789be7f331e67bcd6aa
irchamali <id.irchamali@gmail.com>
2023-11-30 16:53:26 +0700

    Update header of backend

--- app/Views/Layouts/backend-header.php
+++ app/Views/Layouts/backend-header.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?= $setting['nama_website']; ?></title>
+    <title><?= $title; ?> | <?= $setting['nama_website']; ?></title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('public'); ?>/assets/img/favicons/favicon.ico">
     <?php
     if (@$styles) {
         foreach ($styles as $file) {
